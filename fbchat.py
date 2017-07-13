fab158ca022c7f58c2c1a16ac0e496f0ba4b4f14
jybaek <oops.jybaek@gmail.com>
2017-07-13 10:05:09 +0900

    Add chat template

---
+++ fbchat.py
@@ -0,0 +1,18 @@
+# encoding=utf-8
+
+from fbchat import Client
+from fbchat.models import *
+
+client = Client('id', 'password') # XXX.
+
+"""
+client.sendMessage('hi me!', thread_id=client.uid)
+exit(255)
+"""
+
+friends = client.searchForUsers('friend_name')[0] # XXX.
+
+msg = 'jobs message' # XXX.
+
+client.sendMessage(msg, thread_id=friends.uid, thread_type=ThreadType.USER)
+client.logout()
